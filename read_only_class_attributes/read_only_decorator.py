e77c2f219011a617030c5edf1fc64194f376f416
pavanchhatpar <pavanchhatpar@gmail.com>
2020-02-01 18:10:16 -0500

    linting fixes

--- read_only_class_attributes/read_only_decorator.py
+++ read_only_class_attributes/read_only_decorator.py
@@ -1,22 +1,24 @@
 def read_only(*attrs):
     """
-    Class decorator to mark selected attributes of a class as read-only. All attributes
-    in ``attrs`` cannot be modified. If ``*`` is present in ``attrs`` then no attribute
-    can be modified.
+    Class decorator to mark selected attributes of a class as read-only. All
+    attributes in ``attrs`` cannot be modified. If ``*`` is present in
+    ``attrs`` then no attribute can be modified.
 
     Parameters
     ----------
     attrs : list of str
-        Names of the attributes that should be constants. '*' value will make all 
-        attributes constant
+        Names of the attributes that should be constants. '*' value will
+        make all attributes constant
     """
     def _rebuilt_class(cls):
         class ReadOnlyPropertyClass(cls):
             def __setattr__(self, name, value):
                 if "*" in attrs:
-                    raise AttributeError("All attributes of this class are read-only")
+                    raise AttributeError(
+                            "All attributes of this class are read-only")
                 if name in attrs:
-                    raise AttributeError(f"Cannot modify `{name}` as it is marked as read-only")
+                    raise AttributeError(
+                        f"Cannot modify `{name}` as it is marked as read-only")
                 return super().__setattr__(name, value)
         return ReadOnlyPropertyClass
     return _rebuilt_class
